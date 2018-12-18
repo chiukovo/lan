5f9556e490c690f5a4b2985f977b200d1bb3ed6f
chiukovo <q8156697@gmail.com>
2018-12-19 00:59:22 +0800

    1219

--- wp-content/plugins/wp-user-frontend/templates/login-form.php
+++ wp-content/plugins/wp-user-frontend/templates/login-form.php
@@ -18,40 +18,53 @@
     <?php wpuf()->login->show_messages(); ?>
 
     <form name="loginform" class="wpuf-login-form" id="loginform" action="<?php echo $action_url; ?>" method="post">
-        <p>
-            <label for="wpuf-user_login">店家ID</label>
-            <input type="text" name="log" id="wpuf-user_login" class="input" value="" size="20" />
-        </p>
-        <p>
-            <label for="wpuf-user_pass"><?php _e( 'Password', 'wp-user-frontend' ); ?></label>
-            <input type="password" name="pwd" id="wpuf-user_pass" class="input" value="" size="20" />
-        </p>
-
+        <div class="form-group">
+            <div class="wpuf-label">
+                <label for="wpuf-user_login">店家ID</label>
+            </div>
+            <div class="wpuf-fields">
+                <input type="text" name="log" id="wpuf-user_login" class="input" value="" size="20" />
+            </div>
+        </div>
+        <div class="form-group">
+            <div class="wpuf-label">
+                <label for="wpuf-user_pass"><?php _e( 'Password', 'wp-user-frontend' ); ?></label>
+            </div>
+            <div class="wpuf-fields">
+                <input type="password" name="pwd" id="wpuf-user_pass" class="input" value="" size="20" />
+            </div>
+        </div>
+        
         <?php $recaptcha = wpuf_get_option( 'login_form_recaptcha', 'wpuf_profile', 'off'); ?>
         <?php if( $recaptcha == 'on' ) : ?>
-            <p>
+            <p >
                 <div class="wpuf-fields">
                     <?php echo recaptcha_get_html( wpuf_get_option( 'recaptcha_public', 'wpuf_general' ), true, null, is_ssl() ); ?>
                 </div>
             </p>
         <?php endif; ?>
 
-        <p class="forgetmenot">
+        <div class="forgetmenot">
             <input name="rememberme" type="checkbox" id="wpuf-rememberme" value="forever" />
             <label for="wpuf-rememberme">記住我的資訊</label>
-        </p>
-
-        <p class="submit">
+        </div>
+        <div class="wpuf-submit submit">
             <input type="submit" name="wp-submit" id="wp-submit" value="<?php esc_attr_e( 'Log In', 'wp-user-frontend' ); ?>" />
             <input type="hidden" name="redirect_to" value="<?php echo wp_get_referer() ?>" />
             <input type="hidden" name="wpuf_login" value="true" />
             <input type="hidden" name="action" value="login" />
             <?php wp_nonce_field( 'wpuf_login_action' ); ?>
-        </p>
+        </div>
         <p>
             <?php do_action( 'wpuf_login_form_bottom' ); ?>
         </p>
     </form>
+    <div class="login-link">
+        <?php echo wpuf()->login->get_action_links( array( 'login' => false ) ); ?>
+    </div>
+    
 
-    <?php echo wpuf()->login->get_action_links( array( 'login' => false ) ); ?>
+    <script>
+        $('html').attr('id', 'login');
+    </script>
 </div>
